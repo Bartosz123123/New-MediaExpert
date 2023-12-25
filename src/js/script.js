6360b06c5b037292a7cbe83b49b007cf2d3b5d66
Bartosz <bartosz.opalka10@wp.pl>
2023-12-25 18:57:35 +0100

    section fav and basket ended

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const btn = document.querySelector('.search-btn');
 	const navBtn = document.querySelector('.buy');
 	const basketBox = document.querySelector('.buy-box');
-	const iconsBtn = document.querySelectorAll('.icon');
+	// const iconsBtn = document.querySelectorAll('.icon');
 	const activeSidebar = document.querySelector('.toggle-sidebar');
 	const sidebar = document.querySelector('.sidebar');
 	const showMoreBox = sidebar.querySelector('.show-more-box');
@@ -18,17 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
 	const money = document.querySelector('.money');
 	const iphoneName = document.querySelector('.iphone-name');
 	const backArrow = document.querySelectorAll('.back-arrow');
-	const addToBasketBtn = document.querySelectorAll('.favorite');
+	const addToFavoriteBtn = document.querySelectorAll('.favorite');
 
 	const arrow = sidebar.querySelector('.arrow');
-	const title = sidebar.querySelector('.title');
+	// const title = sidebar.querySelector('.title');
 	const slider = sidebar.querySelector('.slider');
 	const themeText = sidebar.querySelector('.theme-text');
 	const changeThemeBtn = sidebar.querySelector('.change-theme');
+	const notification = document.querySelector('.notification');
+	const favNotification = document.querySelector('.fav-notification');
 
 	const basket = document.querySelector('.basket-box');
 	const basketContainer = document.querySelector('.basket-container');
 	const addBasketBtn = document.querySelectorAll('.add-basket-btn');
+
+	const showFavBoxBtn = document.querySelector('.favorite-card');
+	const favBox = document.querySelector('.favorite-box');
+	const favContainer = document.querySelector('.fav-container');
 
 	const activeBuyWindow = (e) => {
 		if (basket.style.display !== 'block') {
@@ -38,18 +44,25 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	};
 
+	const showFavBox = (e) => {
+		if (favBox.style.display !== 'block') {
+			setTimeout(() => {
+				favBox.style.display = 'block';
+			}, 1);
+		}
+	};
+
 	const addItemToBasket = (e) => {
-		// basket.style.display = 'block';
 		const closestLi = e.target.closest('li');
 		const closestImg = closestLi.querySelector('img').src;
 		const closestAlt = closestLi.querySelector('img').alt;
 		const closestPrice = closestLi.querySelector('.price').textContent;
 		const closestName = closestLi.querySelector('.product-name').textContent;
-
 		const newLi = document.createElement('li');
 		newLi.classList.add('basket-item');
 
 		newLi.innerHTML = `
+		<button class="close-item"><i class="fa-solid fa-xmark"></i></button>
 		<div class="basket-img-box">
 			<img src="${closestImg}" alt="${closestAlt}" class="basket-img">
 			<h3 class="basket-title">${closestName}</h3>
@@ -59,9 +72,134 @@ document.addEventListener('DOMContentLoaded', function () {
 			<p class="basket-price">${closestPrice}</p>
 		</div>
 		`;
-		console.log(newLi);
 
 		basketContainer.appendChild(newLi);
+		const allLiInBasket = document.querySelectorAll('.basket-item');
+		getAllElementsInBasket();
+
+		if (allLiInBasket.length === 0) {
+			console.log('Dodaj zakupy do koszyka');
+		} else if (allLiInBasket.length > 0) {
+			notification.textContent = allLiInBasket.length;
+			notification.classList.add('show-notification');
+			notification.classList.remove('close-notification');
+
+			if (allLiInBasket.length >= 10) {
+				notification.textContent = '+9';
+			}
+		}
+	};
+
+	const getAllElementsInFav = () => {
+		const closeLi = document.querySelectorAll('.close-item');
+		if (closeLi !== null && closeLi.length > 0) {
+			closeLi.forEach((btn) => {
+				btn.addEventListener('click', (e) => {
+					e.stopPropagation(); // Zapobiegnij bąbelkowaniu zdarzenia
+
+					if (favBox) {
+						const targetClick = e.target;
+						const targetClicked = targetClick.closest('li');
+						// const favElement = targetClicked.querySelector('.fav-title');
+						const allProduct = document.querySelectorAll('.product-name');
+
+						allProduct.forEach((names) => {
+							const favElement = targetClicked.querySelector('.fav-title'); // Przeniesienie wewnątrz pętli
+							const favElementName = favElement;
+							const allNames = names.textContent;
+							const allLi = names.closest('li');
+
+							if (allLi.classList.contains('added-to-fav')) {
+								if (favElementName.textContent === allNames) {
+									const buttonElement = allLi.querySelector('.favorite');
+
+									if (buttonElement) {
+										// Tutaj możesz wykonywać operacje na elemencie buttonElement
+										buttonElement.classList.remove('added-to-favorite');
+									}
+
+									// Usunięcie elementu li
+									targetClicked.remove();
+
+									// Usunięcie klasy 'added-to-fav' z elementu li
+									allLi.classList.remove('added-to-fav');
+								}
+							}
+						});
+
+						// Sprawdź, czy w koszyku są jeszcze elementy
+
+						const allFavLi = document.querySelectorAll('.fav-li');
+						if (allFavLi.length === 0) {
+							favBox.style.display = 'none';
+							favNotification.classList.add('close-notification');
+
+							setTimeout(() => {
+								favNotification.classList.remove('show-notification');
+								favNotification.classList.remove('close-notification');
+							}, 1000);
+
+							favNotification.textContent = '';
+						} else if (allFavLi.length !== 0) {
+							favNotification.textContent = allFavLi.length;
+
+							if (allFavLi.length >= 10) {
+								favNotification.textContent = '9+';
+							}
+						}
+
+						console.log('ok');
+					}
+				});
+			});
+		}
+	};
+
+	const getAllElementsInBasket = () => {
+		const closeLi = document.querySelectorAll('.close-item');
+		if (closeLi !== null && closeLi.length > 0) {
+			closeLi.forEach((btn) => {
+				btn.addEventListener('click', (e) => {
+					e.stopPropagation(); // Zapobiegnij bąbelkowaniu zdarzenia
+
+					if (basket !== null && basket !== undefined) {
+						// <--- Zmiana
+						const target = e.target;
+						const closestLi = target.closest('li');
+						console.log(closestLi);
+						closestLi.remove();
+
+						const allLiInBasket = document.querySelectorAll('.basket-item');
+						if (allLiInBasket.length === 0) {
+							basket.style.display = 'none';
+							notification.classList.add('close-notification');
+
+							setTimeout(() => {
+								notification.classList.remove('show-notification');
+								notification.classList.remove('close-notification');
+							}, 1000);
+
+							notification.textContent = '';
+						} else if (allLiInBasket.length !== 0) {
+							notification.textContent = allLiInBasket.length;
+
+							if (allLiInBasket.length >= 10) {
+								notification.textContent = '9+';
+							}
+						}
+					}
+
+					// addToFavoriteBtn.forEach((btn, e) => {
+					// 	if (btn.classList.contains('added-to-favorite')) {
+					// 		const allProductWithFav =
+					// 			document.querySelectorAll('.added-to-fav');
+					// 		btn.classList.remove('added-to-favorite');
+					// 		console.log(allProductWithFav);
+					// 	}
+					// });
+				});
+			});
+		}
 	};
 
 	const closePriceBox = () => {
@@ -149,6 +287,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	};
 	//=========================================================//
+
 	shoppingCardBtn.addEventListener('click', activeBuyWindow);
 	addBasketBtn.forEach((addBtn) => {
 		addBtn.addEventListener('click', (e) => {
@@ -168,7 +307,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	document.addEventListener('click', (e) => {
 		if (basketBox.classList.contains('buy-box-active')) {
-			if (!basketBox.contains(e.target)) {
+			if (!e.target.closest('.close-item') && !basketBox.contains(e.target)) {
 				basketBox.classList.remove('buy-box-active');
 			}
 		}
@@ -178,7 +317,14 @@ document.addEventListener('DOMContentLoaded', function () {
 				basket.style.display = 'none';
 			}
 		}
+
+		if (favBox.style.display === 'block') {
+			if (!favBox.contains(e.target)) {
+				favBox.style.display = 'none';
+			}
+		}
 	});
+
 	backArrow.forEach((item) => {
 		item.addEventListener('click', (e) => {
 			const backLi = e.target.closest('li');
@@ -195,6 +341,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		btn.addEventListener('click', showPriceBox);
 	});
 
+	// products.forEach((product) => {
+	// 	product.addEventListener('click', (e) => {
+	// 		if (e.target.closest('li').classList.contains('active-info-card')) {
+	// 			return;
+	// 		} else {
+	// 			product.classList.add('active-info-card');
+	// 		}
+	// 		// product.classList.toggle('active-info-card');
+	// 	});
+	// });
+
 	products.forEach((product) => {
 		product.addEventListener('click', (e) => {
 			if (e.target.closest('li').classList.contains('active-info-card')) {
@@ -202,13 +359,58 @@ document.addEventListener('DOMContentLoaded', function () {
 			} else {
 				product.classList.add('active-info-card');
 			}
-			// product.classList.toggle('active-info-card');
 		});
 	});
-	addToBasketBtn.forEach((btn) => {
+
+	showFavBoxBtn.addEventListener('click', showFavBox);
+
+	addToFavoriteBtn.forEach((btn) => {
 		btn.addEventListener('click', (e) => {
 			const backLi = e.target.closest('li');
-			console.log('dodano do ulubionych');
+
+			// if (backLi.classList.contains('added-to-fav')) {
+			// 	return;
+			// }
+
+			if (!backLi.classList.contains('added-to-fav')) {
+				backLi.classList.add('added-to-fav');
+				const closestImg = backLi.querySelector('img').src;
+				const closestAlt = backLi.querySelector('img').alt;
+				const closestPrice = backLi.querySelector('.price').textContent;
+				const closestName = backLi.querySelector('.product-name').textContent;
+				const newFav = document.createElement('li');
+				newFav.classList.add('fav-li');
+
+				newFav.innerHTML = `
+			<button class="close-item"><i class="fa-solid fa-xmark"></i></button>
+	    		<div class="fav-img-box">
+	       			<img src="${closestImg}" alt="${closestAlt}" class="fav-img">
+
+	        		<h3 class="fav-title">${closestName}</h3>
+	   			</div>
+
+	    		<div class="fav-price-box">
+	             	<p class="fav-price">${closestPrice}</p>
+	    		</div>
+			`;
+				favContainer.appendChild(newFav);
+			}
+			const allFavLi = document.querySelectorAll('.fav-li');
+			getAllElementsInFav();
+			btn.classList.add('added-to-favorite');
+
+			if (allFavLi.length === 0) {
+				console.log('Dodaj do ulubionych');
+			} else if (allFavLi.length > 0) {
+				favNotification.textContent = allFavLi.length;
+				favNotification.classList.add('show-notification');
+				favNotification.classList.remove('close-notification');
+
+				if (allFavLi.length >= 10) {
+					favNotification.textContent = '+9';
+				}
+			}
+
 			setTimeout(() => {
 				backLi.classList.remove('active-info-card');
 			}, 1);
